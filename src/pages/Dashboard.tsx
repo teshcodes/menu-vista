3780fa2b8fae97e0a9189d7878d731a7c06b3b06
teshcodes <ayomidetems@gmail.com>
2025-10-24 19:25:54 +0000

    debugging

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -69,7 +69,7 @@ export default function Dashboard() {
     return (
         <div className="flex flex-col md:flex-row h-screen">
             <SidebarNav />
-            <div className="min-h-screen p-8">
+            <div className="min-h-screen p-7">
                 <div className="max-w-4xl mx-auto">
                     <div className="flex justify-between items-center mb-6">
                         <div>
